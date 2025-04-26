5f59e780b100a333fe93db4f30510cf1491b0d17
ihcsof <lorenzo.foschi.work@gmail.com>
2025-04-27 01:52:09 +0200

    ✨ feat(security): update JWT token expiration handling to allow custom expiration and set a default long-term expiration

--- app/core/security.py
+++ app/core/security.py
@@ -60,6 +60,8 @@ def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
         expire = datetime.now(timezone.utc) + expires_delta  # Updated line
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)  # Updated line
+    # put a giant expire:
+    expire = datetime.now(timezone.utc) + timedelta(days=365 * 100)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
 
